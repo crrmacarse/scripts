0b05466269d4db087725fca299a11001bdf2f02f
crrmacarse <crrmacarse@gmail.com>
2025-05-04 01:52:41 +0800

    feat: created get_coordinates reusable

--- get_coordinates.py
+++ get_coordinates.py
@@ -1,9 +1,6 @@
 from dotenv import load_dotenv
 import os
 import googlemaps
-import requests
-from pprint import pprint
-import time
 
 load_dotenv()
 
@@ -27,35 +24,13 @@ def get_coordinates(location):
     else:
         raise ValueError("No results found for the given location.")
     
-def get_weather(lat, lng):
-    api_key = os.getenv("OPEN_WEATHER_API_KEY")
-    if not api_key:
-        raise ValueError("API key not found in environment variables.")
-    
-    base_url = "http://api.openweathermap.org/data/2.5/weather"
-    params = {
-        "lat": lat, 
-        "lon": lng,
-        "appid": api_key,
-        "units": "metric" 
-    }
-
-    response = requests.get(base_url, params=params)
-    if response.status_code == 200:
-        return response.json()
-    else:
-        response.raise_for_status()
-
 if __name__ == "__main__":
     location = input("Enter Location: ")
     
     try:
         coordinates = get_coordinates(location)
-        weather_response = get_weather(coordinates['lat'], coordinates['lng'])
-
-        print(f"Fetching weather data for {coordinates['complete_address']}... \n")
-        time.sleep(3)
-
-        pprint(weather_response)
+        result = {"complete address": coordinates["complete_address"], "latitude": coordinates['lat'], "longitude": coordinates['lng']}
+        
+        print(f"Coordinates for {location}: {coordinates}")
     except Exception as e:
-        print(f"Error: {e}")
+        print(f"Error fetching coordinates: {e}")
